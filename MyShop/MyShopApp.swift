da71abc0a4c59318fb76b5e942851f9d0296145e
Tuan <tuando22092000@gmail.com>
2022-05-19 16:52:29 +0700

    Initial Commit

---
+++ MyShop/MyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopApp.swift
+//  MyShop
+//
+//  Created by Do Tuan on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
